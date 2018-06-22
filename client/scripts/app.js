deb1e1be409a1b4df51cc583871ff40e38f2a922
kristie-zheng <miyabi3600@gmail.com>
2018-06-22 16:50:19 -0700

    Append messages succesfully

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -21,23 +21,47 @@ var postMessage = () => {
   });
 };
 
-var optionsObject = { order: '-createdAt'};
+
+var parse = (obj) => {
+  console.log(obj);
+
+  for (var i = 0; i < obj.results.length; i++) { // key === 0 or key === 5
+    var id = obj.results[i].objectId;
+    var username = obj.results[i].username;
+    var roomname = obj.results[i].roomname;
+    var text = obj.results[i].text;
+    var createdAt = obj.results[i].createdAt;
+    var str = `Username: ${username}, Text: ${text}, Created At: ${createdAt}`;
+    var $div = $(`<div class = "message"> ${str} </div>`);
+    $('body').append($div);
+  }
+};
+
+var optionsObject = { order: '-createdAt', limit: 20};
 
 var fetch = () => { 
   $.get('http://parse.sfm6.hackreactor.com/chatterbox/classes/messages', optionsObject, function(data) {
-    console.log(data);
+    parse(data);
   });
 };
 
+// var parse = (obj) => {
+//   console.log(obj);
+//   for (var i = 0; i < obj.results.length; i++) { // key === 0 or key === 5
+//     var id = obj.results[i].objectId;
+//     var username = obj.results[i].username;
+//     var roomname = obj.results[i].roomname;
+//     var text = obj.results[i].text;
+//     var createdAt = obj.results[i].createdAt;
+//     var str = `Username: ${username}, Text: ${text}, Created At: ${createdAt}`;
+//     var $div = $(`<div>${str}</div>`)
+//     $('body').append($div);
+//   }
+// }
+
 $(document).ready(function () {
-  for (var i = 0; i < 1000; i++) {
-    setTimeout(fetch, 100);
-    if (i % 50 === 0) {
-      postMessage();
-    }
+  for (var i = 0; i < 5; i++) {
+    postMessage();
   }
-
-  
-  
-
+  fetch();
 });
