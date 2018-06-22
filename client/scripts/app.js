67dfbf66bd16a034609345fa3163763f6ec09b87
kristie-zheng <miyabi3600@gmail.com>
2018-06-22 16:02:01 -0700

    Basic fetch and post functionality

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,10 +1,43 @@
 var message = {
-  username: 'gregP',
+  username: '******************************************************************',
   text: 'checking if this shows up',
   roomname: 'whatever'
 };
+
+var postMessage = () => {
+  $.ajax({
+  // This is the url you should use to communicate with the parse API server.
+    url: 'http://parse.sfm6.hackreactor.com/chatterbox/classes/messages',
+    type: 'POST',
+    data: JSON.stringify(message),
+    contentType: 'application/json',
+    success: function (data) {
+      console.log('chatterbox: Message sent');
+    },
+    error: function (data) {
+      // See: https://developer.mozilla.org/en-US/docs/Web/API/console.error
+      console.error('chatterbox: Failed to send message', data);
+    }
+  });
+};
+
+var optionsObject = { order: '-createdAt'};
+
+var fetch = () => { 
+  $.get('http://parse.sfm6.hackreactor.com/chatterbox/classes/messages', optionsObject, function(data) {
+    console.log(data);
+  });
+};
+
 $(document).ready(function () {
-  // alert("loades")
+  for (var i = 0; i < 1000; i++) {
+    setTimeout(fetch, 100);
+    if (i % 50 === 0) {
+      postMessage();
+    }
+  }
+
+  
   
 
 });
