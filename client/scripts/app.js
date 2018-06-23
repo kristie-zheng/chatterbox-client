f38e4039992a03653978b2be0340bb83a39b44ef
kristie-zheng <miyabi3600@gmail.com>
2018-06-22 18:52:32 -0700

    refactored to ES6

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,67 +1,64 @@
-var message = {
-  username: '******************************************************************',
-  text: 'checking if this shows up',
-  roomname: 'whatever'
-};
+class App {
+  constructor() {
+    this.optionsObject = { order: '-createdAt', limit: 20};
+  }
 
-var postMessage = () => {
-  $.ajax({
-  // This is the url you should use to communicate with the parse API server.
-    url: 'http://parse.sfm6.hackreactor.com/chatterbox/classes/messages',
-    type: 'POST',
-    data: JSON.stringify(message),
-    contentType: 'application/json',
-    success: function (data) {
-      console.log('chatterbox: Message sent');
-    },
-    error: function (data) {
-      // See: https://developer.mozilla.org/en-US/docs/Web/API/console.error
-      console.error('chatterbox: Failed to send message', data);
+  postMessage() {
+    $.ajax({
+      url: 'http://parse.sfm6.hackreactor.com/chatterbox/classes/messages',
+      type: 'POST',
+      data: JSON.stringify(message),
+      contentType: 'application/json',
+      success: function (data) {
+        console.log('chatterbox: Message sent');
+      },
+      error: function (data) {
+        // See: https://developer.mozilla.org/en-US/docs/Web/API/console.error
+        console.error('chatterbox: Failed to send message', data);
+      }
+    });
+  }
+  parse (obj) {
+    console.log(obj);
+    for (var i = 0; i < obj.results.length; i++) { // key === 0 or key === 5
+      var id = obj.results[i].objectId;
+      var username = obj.results[i].username;
+      var roomname = obj.results[i].roomname;
+      var text = obj.results[i].text;
+      var createdAt = obj.results[i].createdAt;
+      var str = `Username: ${username}, Text: ${text}, Created At: ${createdAt}`;
+      var $div = $(`<div class = "message"> ${str} </div>`);
+      $('body').append($div);
     }
-  });
-};
-
-
-var parse = (obj) => {
-  console.log(obj);
-
-  for (var i = 0; i < obj.results.length; i++) { // key === 0 or key === 5
-    var id = obj.results[i].objectId;
-    var username = obj.results[i].username;
-    var roomname = obj.results[i].roomname;
-    var text = obj.results[i].text;
-    var createdAt = obj.results[i].createdAt;
-    var str = `Username: ${username}, Text: ${text}, Created At: ${createdAt}`;
-    var $div = $(`<div class = "message"> ${str} </div>`);
-    $('body').append($div);
   }
-};
-
-var optionsObject = { order: '-createdAt', limit: 20};
-
-var fetch = () => { 
-  $.get('http://parse.sfm6.hackreactor.com/chatterbox/classes/messages', optionsObject, function(data) {
-    parse(data);
-  });
-};
-
-// var parse = (obj) => {
-//   console.log(obj);
-//   for (var i = 0; i < obj.results.length; i++) { // key === 0 or key === 5
-//     var id = obj.results[i].objectId;
-//     var username = obj.results[i].username;
-//     var roomname = obj.results[i].roomname;
-//     var text = obj.results[i].text;
-//     var createdAt = obj.results[i].createdAt;
-//     var str = `Username: ${username}, Text: ${text}, Created At: ${createdAt}`;
-//     var $div = $(`<div>${str}</div>`)
-//     $('body').append($div);
-//   }
-// }
-
-$(document).ready(function () {
-  for (var i = 0; i < 5; i++) {
-    postMessage();
+  fetch () { 
+    $.get('http://parse.sfm6.hackreactor.com/chatterbox/classes/messages', this.optionsObject, function(data) {
+      parse(data);
+    });
   }
-  fetch();
-});
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// $(document).ready(function () {
+//   // for (var i = 0; i < 5; i++) {
+//   //   postMessage();
+//   // }
+//   // fetch();
+var app = new App();
+
+// });
